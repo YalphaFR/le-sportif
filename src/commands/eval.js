c92049c3b25166ef579ff0d2fa97b2362f322dc0
yalpha <44028028+YalphaFR@users.noreply.github.com>
2021-07-04 13:38:01 +0200

    Create eval.js

---
+++ src/commands/eval.js
@@ -0,0 +1,40 @@
+const bot = require("../bot");
+const config = require("../config");
+
+const evaluate = bot.registerCommand("eval", async (msg, args) => {
+    let res;
+    try {
+        res = await eval(args.join(" "));
+    } catch (err) {
+        res = err.message;
+    }
+    return msg.channel.createMessage({embed: {
+        fields: [
+                    {
+                        name: "Commande",
+                        value: "```js\n" + args.join(" ") + "```"
+                    },
+                    {
+                        name: "Résultat",
+                        value: "```js\n" + formatPrint(res)  + "```",
+                    }
+                ],
+    }}).catch(() => {console.log("Une erreur s'est produite en tentant d'évaluer une expression")});
+}, {
+    argsRequired: true,
+    requirements: {
+        userIDs: [config.ownerID],
+        permissions: {
+            administrator: true,
+        }
+    }
+});
+
+function formatPrint(value) {
+    const typeValue = typeof value;
+    if (typeValue === typeof {} || typeValue === typeof []) {
+        return String(value);
+    }
+    return value;
+}
+
