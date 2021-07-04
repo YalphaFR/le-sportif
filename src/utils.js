621f2c4a4992b11fb98102c2f377ebc510557872
yalpha <44028028+YalphaFR@users.noreply.github.com>
2021-07-04 11:43:42 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -31,9 +31,24 @@ function date() {
     return moment().format("LLLL");
 }
 
+function randomInt(min, max) {
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(ms) {
+    return new Promise((res) => setTimeout(res, ms));
+}
+
+async function getRESTMembers(guild) {
+    return guild.getRESTMembers({limit: 1000});
+}
+
 module.exports = {
     getMainRole,
     isStaff,
     isBenevole,
     date,
+    randomInt,
+    sleep,
+    getRESTMembers,
 };
