39aa1c6368db41d0bb3f9140bb2f7783cacd66e0
yalpha <44028028+YalphaFR@users.noreply.github.com>
2021-07-08 22:43:08 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,10 @@ const config = require("./config");
 
 const Eris = require("eris");
 const bot = new Eris.CommandClient(config.token, {
-
-}, {
+    intents: Object.keys(Eris.Constants.Intents),
+    restMode: true
+    }, 
+    {
     caseInsensitive: true,
     guildOnly: true,
 });
